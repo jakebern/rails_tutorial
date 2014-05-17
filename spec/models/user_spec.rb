a00aca178a055b29b16428302d8d4c2e926d3058
jacobjbernstein@gmail.com <jacobjbernstein@gmail.com>
2014-05-17 16:19:40 -0500

    Done with 8.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,37 +5,38 @@ describe User do
 	before { @user = User.new(name: "Example User", email: "user@example.com",
     password: "foobar", password_confirmation: "foobar")}
 
-	subject { @user }
+  	subject { @user }
 
-	it { should respond_to(:name)}
-	it { should respond_to (:email)}
-	it { should respond_to (:password_digest)}
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+  	it { should respond_to(:name)}
+  	it { should respond_to (:email)}
+  	it { should respond_to (:password_digest)}
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
 
-	it { should be_valid }
+  	it { should be_valid }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
+    describe "when name is not present" do
+      before { @user.name = " " }
+      it { should_not be_valid }
+    end
 
-  describe "when name is too long" do
-  	before { @user.name = "a" * 51 }
-  	it { should_not be_valid}
-  end
+    describe "when name is too long" do
+    	before { @user.name = "a" * 51 }
+    	it { should_not be_valid}
+    end
 
 
-  describe "when email is not present" do
-  	before { @user.email = " "}
-  	it { should_not be_valid}
-  end
+    describe "when email is not present" do
+    	before { @user.email = " "}
+    	it { should_not be_valid}
+    end
 
-describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                       foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -59,15 +60,16 @@ describe "when email format is invalid" do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-		
-	 it { should_not be_valid }
-	end
+  		
+  	it { should_not be_valid }
+  end
 
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
+                         password: " ", password_confirmation: " ")
     end
+    
     it { should_not be_valid }
   end
 
@@ -86,7 +88,7 @@ describe "when email format is invalid" do
     let(:found_user) { User.find_by(email: @user.email) }
 
     describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
+        it { should eq found_user.authenticate(@user.password) }
     end
 
     describe "with invalid password" do
@@ -95,5 +97,10 @@ describe "when email format is invalid" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
